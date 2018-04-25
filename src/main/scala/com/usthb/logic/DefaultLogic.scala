adb8979e228e0ef2aa05079c73c6f1c11b480040
mohammedi <hm@big-mama.io>
2018-04-26 00:06:06 +0100

    return the generators along with the extension

--- src/main/scala/com/usthb/logic/DefaultLogic.scala
+++ src/main/scala/com/usthb/logic/DefaultLogic.scala
@@ -8,7 +8,7 @@ import scala.language.postfixOps
 /** A Default in logic default has three parts : prerequis, justificatifs and consequence
   *
   *   1. a default is applicable for a world W when the ''`World |= prerequis`''
-  *   2. an applicable default is usable when for all j`'' from justificatifs ''` World |= !j`'' is false
+  *   2. an applicable default is usable when for all ''`j`'' from justificatifs ''`World |= !j`'' is false
   *
   * We highly recommend you to use the provided dsl syntax to define Defalut (you should import com.usthb.logic.Default._)
   *
@@ -22,14 +22,19 @@ import scala.language.postfixOps
 case class Default(prerequis: Formula,
                    justificatifs: FormulaSet,
                    consequence: Formula) {
-  def isApplicable(world: FormulaSet): Boolean = world |= prerequis
+  val num: Int = Default.num
+  Default.num = Default.num + 1
+  def isApplicable(world: FormulaSet): Boolean = if (prerequis == Empty) true else world |= prerequis
   def isUsable(world: FormulaSet): Boolean =
     justificatifs.map(Negation.apply).find(world |= _) match {
       case Some(_) => false
       case None    => true
     }
+
+  override def toString: String = s"d${num}"
 }
 object Default {
+  var num = 1
   def apply(prerequis: Formula,
             justificatif: Formula,
             consequence: Formula): Default =
@@ -51,24 +56,28 @@ case class Theory(world: FormulaSet, defaults: Array[Default]) {
 
   /**
     * Implemention for extention in defaults logic, refer to default logic lecture for more information
+    * @param order a sequence of [[com.usthb.logic.Default]] that will be executing in the given order
+    * @return the extension found by using the given defaults in order and a list of generators defaults
     */
-  private def extention(order: Seq[Default]): FormulaSet = {
+  private def extention(order: Seq[Default]): (FormulaSet, List[Default]) = {
     require(order.lengthCompare(defaults.length) <= 0,
             "order of defautls should contain for most number of defaults")
     val delta = mutable.Set.empty[Formula]
+    var generators = List.empty[Default]
     delta ++= world
     for (d <- order) {
       if (d.isApplicable(delta) && d.isUsable(delta)) {
-        println(s"d = ${d}")
         delta += d.consequence
+        generators = d :: generators
       }
     }
-    delta
+    (delta, generators.reverse)
   }
 
   /**
     * get all possible extentions for this Theory
+    * @return all extensions associated with their generators
     */
-  def extentions: Set[FormulaSet] =
-    defaults.permutations.map(x => extention(x)).toSet
+  def extentions: Map[FormulaSet, Predef.Set[String]] =
+    defaults.permutations.map(x => extention(x)).toList.groupBy(_._1).mapValues(_.map(_._2.mkString("<")).toSet)
 }
